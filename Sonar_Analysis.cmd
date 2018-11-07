bb3ff837917018c3597a3a798f4f274a5e3ed2be
Jonas Wälter <waelter@outlook.de>
2018-11-07 15:52:43 +0100

    Fix sonarqube test coverage

--- Sonar_Analysis.cmd
+++ Sonar_Analysis.cmd
@@ -1,2 +1,2 @@
-.\gradlew.bat sonarqube -Dsonar.host.url=http://localhost:9000 -Dsonar.login=414a31402b82e85601cc2b5491aa28ed9ac3b5df
+.\gradlew.bat clean jacocoTestReport sonarqube -Dsonar.host.url=http://localhost:9000 -Dsonar.login=414a31402b82e85601cc2b5491aa28ed9ac3b5df
 pause
