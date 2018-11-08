521132485d72220460faff0b5e4386a4a5fa4d06
Jonas Wälter <waelter@outlook.de>
2018-11-08 10:21:08 +0100

    Set theme for sign in activity

--- app/src/main/java/ch/hsr/winescore/ui/profile/ProfilePresenter.java
+++ app/src/main/java/ch/hsr/winescore/ui/profile/ProfilePresenter.java
@@ -8,6 +8,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
+import ch.hsr.winescore.R;
 import ch.hsr.winescore.data.repositories.CommentsFirebaseRepository;
 import ch.hsr.winescore.data.repositories.FavoritesFirebaseRepository;
 import ch.hsr.winescore.data.repositories.RatingsFirebaseRepository;
@@ -41,6 +42,7 @@ public class ProfilePresenter implements Presenter<ProfileView> {
                         new AuthUI.IdpConfig.AnonymousBuilder().build()
                 ))
                 .setIsSmartLockEnabled(false)
+                .setTheme(R.style.AppTheme)
                 .build();
     }
 
